bd33c51a0b9e25115d6f8a2679dba763d60ac82b
Enrique Escribano <72576090+EnriqueEscribano@users.noreply.github.com>
2020-11-30 16:26:13 +0100

    Update ContadorScript.cs

--- ExamenTeorico/New UnityrtytutProject/Assets/Scripts/ContadorScript.cs
+++ ExamenTeorico/New UnityrtytutProject/Assets/Scripts/ContadorScript.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class ContadorScript : MonoBehaviour
 {
-    [SerializeField] Text MyText;
-    [SerializeField] float speed;
+    [SerializeField] Text TextContador;
     
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine("Contador");
+
     }
     
     // Update is called once per frame
@@ -20,7 +19,7 @@ public class ContadorScript : MonoBehaviour
         {
             for (int n = 0; ; n++)
             {
-                MyText.text = "Tiempo de juego transcurrido: " + n + "s";
+                TextContador.text = "Tiempo de juego transcurrido: " + n + "s";
              }
         }
      }
